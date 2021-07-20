cdf98b86d8b3d444709b63b4353a6f59173445cc
walfrido-oliveira <walfrido_15@hotmail.com>
2021-07-20 10:31:05 -0300

    Adicionado campo telefone a tabela de usuário

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -36,6 +36,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             $user->forceFill([
                 'name' => $input['name'],
                 'email' => $input['email'],
+                'phone' => $input['phone'],
             ])->save();
         }
 
