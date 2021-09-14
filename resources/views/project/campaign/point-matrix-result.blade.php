9f506742ef290b7e6828b7eafcb691b10e2531ac
walfrido-oliveira <walfrido_15@hotmail.com>
2021-09-13 21:45:49 -0300

    #58

--- resources/views/project/campaign/point-matrix-result.blade.php
+++ resources/views/project/campaign/point-matrix-result.blade.php
@@ -1,12 +1,7 @@
 <thead>
     <tr class="thead-light">
         <x-table-sort-header :orderBy="null" :ascending="null" columnName="" columnText="<input class='form-checkbox' id='select_all_point_matrix' type='checkbox' value='all'>"/>
-        <x-table-sort-header :orderBy="$orderBy" :ascending="$ascending" columnName="area" columnText="{{ __('Área') }}"/>
-        <x-table-sort-header :orderBy="$orderBy" :ascending="$ascending" columnName="identification" columnText="{{ __('Ponto') }}"/>
-        <x-table-sort-header :orderBy="$orderBy" :ascending="$ascending" columnName="analysis_matrix_id" columnText="{{ __('Matriz') }}"/>
-        <x-table-sort-header :orderBy="$orderBy" :ascending="$ascending" columnName="plan_action_level_id" columnText="{{ __('Tipo Nível Ação') }}"/>
-        <x-table-sort-header :orderBy="$orderBy" :ascending="$ascending" columnName="guiding_parameter_id" columnText="{{ __('Param. Orientador Ambiental') }}"/>
-        <x-table-sort-header :orderBy="$orderBy" :ascending="$ascending" columnName="parameter_analysis_id" columnText="{{ __('Param. Análise') }}"/>
+        <x-table-sort-header :orderBy="null" :ascending="null" columnName="" columnText="{{ __('') }}"/>
         <th scope="col"
             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
             Ações
@@ -16,7 +11,7 @@
 <tbody id="point_matrix_table_content">
     @forelse ($projectPointMatrices as $key => $projectPointMatrix)
         <tr id="point_matrix_row_{{ $key }}" class="point-matrix-row">
-            <td>
+            <td width="5%">
                 <input class="form-checkbox point-matrix-url" type="checkbox" name="point_matrix[{{ $projectPointMatrix->id }}]" value="{!! route('project.point-matrix.destroy', ['point_matrix' => $projectPointMatrix->id]) !!}" data-id="point_matrix_row_{{ $key }}">
                 <input type="hidden" name="point_matrix[{{ $key }}][id]" id="point_matrix_{{ $key }}_id" value="{{ $projectPointMatrix->id }}">
             </td>
@@ -24,43 +19,71 @@
                 <input type="hidden" name="point_matrix[{{ $key }}][point_identification_id]" id="point_matrix_{{ $key }}_point_identification_id" value="{{ $projectPointMatrix->pointIdentification ? $projectPointMatrix->pointIdentification->id : null }}">
                     <input type="hidden" name="point_matrix[{{ $key }}][area]" id="point_matrix_{{ $key }}_area" value="{{ $projectPointMatrix->pointIdentification ? $projectPointMatrix->pointIdentification->area : null }}">
                 @if ($projectPointMatrix->pointIdentification)
-                    <a class="text-green-600 underline" href="{{ route('registers.point-identification.show', ['point_identification' => $projectPointMatrix->pointIdentification->id]) }}" target="_blank" rel="noopener noreferrer">
-                        {{ $projectPointMatrix->pointIdentification->area }}
-                    </a>
+                    <div class="grid grid-flow-col grid-cols-2 grid-rows-2 gap-4" style="grid-template-rows: repeat(7, minmax(0, 1fr));">
+                        <div class="font-bold text-right">
+                            {{ __('Área') }}
+                        </div>
+                        <div class="font-bold text-right">
+                            {{ __('Ponto') }}
+                        </div>
+                        <div class="font-bold text-right">
+                            {{ __('Matriz') }}
+                        </div>
+                        <div class="font-bold text-right">
+                            {{ __('Tipo Nível Ação Plano') }}
+                        </div>
+                        <div class="font-bold text-right">
+                            {{ __('Param. Orientador Ambiental') }}
+                        </div>
+                        <div class="font-bold text-right">
+                            {{ __('Param. Análise') }}
+                        </div>
+                        <div class="font-bold text-right">
+                            {{ __('Idet Lab Amostra') }}
+                        </div>
+                        <div class="text-gray-500 font-bold">
+                            <a class="text-green-600 underline" href="{{ route('registers.point-identification.show', ['point_identification' => $projectPointMatrix->pointIdentification->id]) }}" target="_blank" rel="noopener noreferrer">
+                                {{ $projectPointMatrix->pointIdentification->area }}
+                            </a>
+                        </div>
+                        <div class="text-gray-500 font-bold">
+                            @if ($projectPointMatrix->pointIdentification)
+                                <a class="text-green-600 underline" href="{{ route('registers.point-identification.show', ['point_identification' => $projectPointMatrix->pointIdentification->id]) }}" target="_blank" rel="noopener noreferrer">
+                                    {{ $projectPointMatrix->pointIdentification->identification }}
+                                </a>
+                            @endif
+                        </div>
+                        <div class="text-gray-500 font-bold">
+                            <input type="hidden" name="point_matrix[{{ $key }}][analysis_matrix_id]" id="point_matrix_{{ $key }}_analysis_matrix_id" value="{{ $projectPointMatrix->analysisMatrix ? $projectPointMatrix->analysisMatrix->id : null }}">
+                            @if ($projectPointMatrix->analysisMatrix)
+                                {{ $projectPointMatrix->analysisMatrix->name }}
+                            @endif
+                        </div>
+                        <div class="text-gray-500 font-bold">
+                            <input type="hidden" name="point_matrix[{{ $key }}][plan_action_level_id]" id="point_matrix_{{ $key }}_plan_action_level_id" value="{{ $projectPointMatrix->planActionLevel ? $projectPointMatrix->planActionLevel->id : null }}">
+                            @if ($projectPointMatrix->planActionLevel)
+                                {{ $projectPointMatrix->planActionLevel->name }}
+                            @endif
+                        </div>
+                        <div class="text-gray-500 font-bold">
+                            <input type="hidden" name="point_matrix[{{ $key }}][guiding_parameter_id]" id="point_matrix_{{ $key }}_guiding_parameter_id" value="{{ $projectPointMatrix->guidingParameter ? $projectPointMatrix->guidingParameter->id : null }}">
+                            @if ($projectPointMatrix->guidingParameter)
+                                {{ $projectPointMatrix->guidingParameter->environmental_guiding_parameter_id }}
+                            @endif
+                        </div>
+                        <div class="text-gray-500 font-bold">
+                            <input type="hidden" name="point_matrix[{{ $key }}][parameter_analysis_id]" id="point_matrix_{{ $key }}_parameter_analysis_id" value="{{ $projectPointMatrix->parameterAnalysis ? $projectPointMatrix->parameterAnalysis->id : null }}">
+                            @if ($projectPointMatrix->parameterAnalysis)
+                                {{ $projectPointMatrix->parameterAnalysis->analysis_parameter_name }}
+                            @endif
+                        </div>
+                        <div class="text-gray-500 font-bold">
+                            {{ '-' }}
+                        </div>
+                    </div>
                 @endif
             </td>
-            <td>
-                @if ($projectPointMatrix->pointIdentification)
-                    <a class="text-green-600 underline" href="{{ route('registers.point-identification.show', ['point_identification' => $projectPointMatrix->pointIdentification->id]) }}" target="_blank" rel="noopener noreferrer">
-                        {{ $projectPointMatrix->pointIdentification->identification }}
-                    </a>
-                @endif
-            </td>
-            <td>
-                <input type="hidden" name="point_matrix[{{ $key }}][analysis_matrix_id]" id="point_matrix_{{ $key }}_analysis_matrix_id" value="{{ $projectPointMatrix->analysisMatrix ? $projectPointMatrix->analysisMatrix->id : null }}">
-                @if ($projectPointMatrix->analysisMatrix)
-                    {{ $projectPointMatrix->analysisMatrix->name }}
-                @endif
-            </td>
-            <td>
-                <input type="hidden" name="point_matrix[{{ $key }}][plan_action_level_id]" id="point_matrix_{{ $key }}_plan_action_level_id" value="{{ $projectPointMatrix->planActionLevel ? $projectPointMatrix->planActionLevel->id : null }}">
-                @if ($projectPointMatrix->planActionLevel)
-                    {{ $projectPointMatrix->planActionLevel->name }}
-                @endif
-            </td>
-            <td>
-                <input type="hidden" name="point_matrix[{{ $key }}][guiding_parameter_id]" id="point_matrix_{{ $key }}_guiding_parameter_id" value="{{ $projectPointMatrix->guidingParameter ? $projectPointMatrix->guidingParameter->id : null }}">
-                @if ($projectPointMatrix->guidingParameter)
-                    {{ $projectPointMatrix->guidingParameter->environmental_guiding_parameter_id }}
-                @endif
-            </td>
-            <td>
-                <input type="hidden" name="point_matrix[{{ $key }}][parameter_analysis_id]" id="point_matrix_{{ $key }}_parameter_analysis_id" value="{{ $projectPointMatrix->parameterAnalysis ? $projectPointMatrix->parameterAnalysis->id : null }}">
-                @if ($projectPointMatrix->parameterAnalysis)
-                    {{ $projectPointMatrix->parameterAnalysis->analysis_parameter_name }}
-                @endif
-            </td>
-            <td>
+            <td width="20%">
                 <div class="edit inline">
                     @include('project.edit-icon', ['key' => $key, 'id' => $projectPointMatrix->id, 'className' => 'edit-point-matrix'])
                 </div>
