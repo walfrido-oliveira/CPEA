9b6ff10f095040b173b3466c22bff82a8882eac8
walfrido-oliveira <walfrido_15@hotmail.com>
2021-09-16 10:50:45 -0300

    correção

--- resources/views/project/point-matrix/show.blade.php
+++ resources/views/project/point-matrix/show.blade.php
@@ -120,8 +120,8 @@
                         <tbody>
                             @foreach ($parameterAnalyses as $key => $parameterAnalysis)
                                 <tr>
-                                    <td>
-                                        <a class="text-green-600 underline text-item-table text-center" href="{{ route('parameter-analysis.show', ['parameter_analysis' => $parameterAnalysis->id]) }}">
+                                    <td class="text-center">
+                                        <a class="text-green-600 underline text-item-table" href="{{ route('parameter-analysis.show', ['parameter_analysis' => $parameterAnalysis->id]) }}">
                                             {{ $parameterAnalysis->cas_rn }} - {{ $parameterAnalysis->analysis_parameter_name }} - {{ $parameterAnalysis->parameterAnalysisGroup ? $parameterAnalysis->parameterAnalysisGroup->name : '' }}</td>
                                         </a>
                                     </td>
