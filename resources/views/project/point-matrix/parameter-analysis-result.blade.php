9699ec90c7333395f84b32438e0bc40df9ca4026
walfrido-oliveira <walfrido_15@hotmail.com>
2021-09-23 10:03:27 -0300

    correção

--- resources/views/project/point-matrix/parameter-analysis-result.blade.php
+++ resources/views/project/point-matrix/parameter-analysis-result.blade.php
@@ -8,21 +8,25 @@
         @if (($index > 0 && $projectPointMatrices[$index]->pointIdentification->identification !=
                             $projectPointMatrices[$index - 1]->pointIdentification->identification) || $index == 0)
             <tr>
-                <td class="font-bold" style="background-color:#e1ede1 ">
+                <td class="font-bold">
                     @if ($point->pointIdentification)
-                        <button type="button" class="point-open pl-4" data-id="{{ $point->pointIdentification->id }}">{{ $point->pointIdentification->area }} - {{ $point->pointIdentification->identification }}</button>
-                        <svg class="fill-current h-4 w-4 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                        </svg>
+                        <button type="button" class="point-open pl-4" data-id="{{ $point->pointIdentification->id }}">
+                            {{ $point->pointIdentification->area }} - {{ $point->pointIdentification->identification }}
+                            <svg class="fill-current h-4 w-4 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                            </svg>
+                        </button>
                     @endif
                 </td>
             </tr>
         @endif
 
         @if (($index > 0 && $projectPointMatrices[$index]->parameterAnalysis->parameter_analysis_group_id !=
-                            $projectPointMatrices[$index - 1]->parameterAnalysis->parameter_analysis_group_id) || $index == 0)
+                            $projectPointMatrices[$index - 1]->parameterAnalysis->parameter_analysis_group_id) || $index == 0 ||
+                            ($projectPointMatrices[$index]->pointIdentification->identification !=
+                            $projectPointMatrices[$index - 1]->pointIdentification->identification))
             <tr class="hidden transition-transform duration-200 transform point-items-{{ $point->pointIdentification->id }}">
-                <td class="font-bold text-black" style="background-color:#ccc">
+                <td class="font-bold text-black" style="background-color:#e1ede1">
                     {{ $point->parameterAnalysis->parameterAnalysisGroup->name }}
                 </td>
             </tr>
