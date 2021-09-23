2d56e78a1248246196059474f73c7a808bb41500
walfrido-oliveira <walfrido_15@hotmail.com>
2021-09-23 10:00:26 -0300

    correção

--- resources/views/project/point-matrix/parameter-analysis-result.blade.php
+++ resources/views/project/point-matrix/parameter-analysis-result.blade.php
@@ -10,7 +10,10 @@
             <tr>
                 <td class="font-bold" style="background-color:#e1ede1 ">
                     @if ($point->pointIdentification)
-                        <button type="button" class="point-open" data-id="{{ $point->pointIdentification->id }}">{{ $point->pointIdentification->area }} - {{ $point->pointIdentification->identification }}</button>
+                        <button type="button" class="point-open pl-4" data-id="{{ $point->pointIdentification->id }}">{{ $point->pointIdentification->area }} - {{ $point->pointIdentification->identification }}</button>
+                        <svg class="fill-current h-4 w-4 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                        </svg>
                     @endif
                 </td>
             </tr>
