5f97050a45265410c2d52f91c8f4976819cc0102
walfrido-oliveira <walfrido_15@hotmail.com>
2021-09-27 10:05:42 -0300

    icone dinamico

--- resources/views/project/point-matrix/parameter-analysis-result.blade.php
+++ resources/views/project/point-matrix/parameter-analysis-result.blade.php
@@ -8,12 +8,15 @@
         @if (($index > 0 && $projectPointMatrices[$index]->pointIdentification->identification !=
                             $projectPointMatrices[$index - 1]->pointIdentification->identification) || $index == 0)
             <tr>
-                <td class="font-bold">
+                <td class="font-bold" x-data="{ open: false }">
                     @if ($point->pointIdentification)
-                        <button type="button" class="point-open pl-4" data-id="{{ $point->pointIdentification->id }}">
+                        <button @click="open = !open" type="button" class="point-open pl-4" data-id="{{ $point->pointIdentification->id }}">
                             {{ $point->pointIdentification->area }} - {{ $point->pointIdentification->identification }}
-                            <svg class="fill-current h-4 w-4 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <svg :class="{'inline': !open, 'hidden': open}" class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                            </svg>
+                            <svg :class="{'inline': open, 'hidden': !open}" xmlns="http://www.w3.org/2000/svg" class="fill-current h-4 w-4 hidden up" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" />
                             </svg>
                         </button>
                     @endif
@@ -49,6 +52,7 @@
 <script>
     document.querySelectorAll(".point-open").forEach(item => {
         item.addEventListener("click", function() {
+
             document.querySelectorAll(".point-items-" + this.dataset.id).forEach(item => {
                 if(item.classList.contains("hidden")) {
                     item.classList.remove("hidden");
