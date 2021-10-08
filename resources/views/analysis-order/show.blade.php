2f67cc6470e79f5771a2bd65da8d9df4dc781db1
walfrido-oliveira <walfrido_15@hotmail.com>
2021-10-08 09:15:34 -0300

    correções no botão

--- resources/views/analysis-order/show.blade.php
+++ resources/views/analysis-order/show.blade.php
@@ -5,6 +5,11 @@
                 <div class="w-full flex items-center">
                     <h1>Pedido {{ $analysisOrder->formatted_id }}</h1>
                 </div>
+                <div class="w-full flex justify-end">
+                    <div class="m-2 ">
+                        <a class="btn-outline-danger" href="{{ route('sample-analysis.show', ['campaign' => $analysisOrder->campaign_id]) }}">{{ __('Voltar') }}</a>
+                    </div>
+                </div>
             </div>
 
             <div id="status_analysis_order">
