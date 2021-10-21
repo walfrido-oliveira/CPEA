1df368e316ca66a9840bd8b4af4fa3f9abf77548
walfrido-oliveira <walfrido_15@hotmail.com>
2021-10-21 10:48:44 -0300

    removido lista

--- resources/views/project/edit.blade.php
+++ resources/views/project/edit.blade.php
@@ -148,7 +148,7 @@
                         <div class="flex flex-wrap mx-4 px-3 py-2">
                             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                                 <x-jet-label for="guiding_parameters_id" value="{{ __('Param. Orientador Ambiental') }}"/>
-                                <x-custom-select multiple :options="$guidingParameters" name="guiding_parameters_id" id="guiding_parameters_id" value="" class="mt-1" no-filter="no-filter"/>
+                                <x-custom-multi-select multiple :options="$guidingParameters" name="guiding_parameters_id" id="guiding_parameters_id" value="" select-class="form-input" class="mt-1" no-filter="no-filter" default="default"/>
                             </div>
                             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                                 <x-jet-label for="analysis_parameter_id" value="{{ __('Param. Análise') }}"/>
