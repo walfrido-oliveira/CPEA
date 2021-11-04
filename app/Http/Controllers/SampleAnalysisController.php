b4df8f0635caec81474265bf2338563e45a90709
walfrido-oliveira <walfrido_15@hotmail.com>
2021-11-04 12:01:48 -0300

    teste

--- app/Http/Controllers/SampleAnalysisController.php
+++ app/Http/Controllers/SampleAnalysisController.php
@@ -43,16 +43,14 @@ class SampleAnalysisController extends Controller
         $status = ['sent' => __('sent'), 'pending' => __('pending'), 'analyzing' => __('analyzing'), 'concluded' => __('concluded')];
         $orders = $campaign->analysisOrders;
 
-        $ascending = isset($query['ascending']) ? $query['ascending'] : 'asc';
-        $orderBy = isset($query['order_by']) ? $query['order_by'] : 'point_identifications.area';
-
         $projectPointMatrices = $campaign
         ->projectPointMatrices()
         ->with('pointIdentification')
         ->leftJoin('point_identifications', 'point_identifications.id', '=', 'project_point_matrices.point_identification_id')
         ->leftJoin('parameter_analyses', 'parameter_analyses.id', '=', 'project_point_matrices.parameter_analysis_id')
         ->leftJoin('parameter_analysis_groups', 'parameter_analysis_groups.id', '=', 'parameter_analyses.parameter_analysis_group_id')
-        ->orderBy($orderBy, $ascending)
+        ->orderBy('point_identifications.area', 'asc')
+        ->orderBy('point_identifications.identification ', 'asc')
         ->orderBy('parameter_analysis_groups.name', 'asc')
         ->select('project_point_matrices.*')
         ->get();
