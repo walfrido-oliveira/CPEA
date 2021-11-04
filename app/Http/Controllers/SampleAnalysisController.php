f51be8b372d38421150e15f2606674b1c5cf7a65
walfrido-oliveira <walfrido_15@hotmail.com>
2021-11-04 09:16:45 -0300

    correção de filtro

--- app/Http/Controllers/SampleAnalysisController.php
+++ app/Http/Controllers/SampleAnalysisController.php
@@ -105,13 +105,47 @@ class SampleAnalysisController extends Controller
      */
     public function filterPointMatrix(Request $request)
     {
-        $inputs = $request->except(['parameter_analysis_item']);
+        $query = $request->except(['parameter_analysis_item']);
 
-        $projectPointMatrices = ProjectPointMatrix::filter($inputs);
         $orderBy = $request->get('order_by');
         $ascending = $request->get('ascending');
+
         $campaign = Campaign::findOrFail($request->get('campaign_id'));
 
+        $projectPointMatrices = $campaign
+        ->projectPointMatrices()
+        ->with('pointIdentification')
+        ->leftJoin('point_identifications', 'point_identifications.id', '=', 'project_point_matrices.point_identification_id')
+        ->leftJoin('parameter_analyses', 'parameter_analyses.id', '=', 'project_point_matrices.parameter_analysis_id')
+        ->leftJoin('parameter_analysis_groups', 'parameter_analysis_groups.id', '=', 'parameter_analyses.parameter_analysis_group_id')
+        ->where(function($q) use ($query) {
+            if(isset($query['q']))
+            {
+                if(!is_null($query['q']))
+                {
+                    $q->where( function($q) use($query){
+                        $q->whereHas('pointIdentification', function($q) use($query) {
+                            $q->where('point_identifications.area', 'like', '%' . $query['q'] . '%')
+                                ->orWhere('point_identifications.identification', 'like', '%' . $query['q'] . '%');
+                        })
+                        ->orWhereHas('analysisMatrix', function($q) use($query) {
+                            $q->where('analysis_matrices.name', 'like', '%' . $query['q'] . '%');
+                        })
+                        ->orWhereHas('guidingParameters', function($q) use($query) {
+                            $q->where('guiding_parameters.environmental_guiding_parameter_id', 'like', '%' . $query['q'] . '%');
+                        })
+                        ->orWhereHas('parameterAnalysis', function($q) use($query) {
+                            $q->where('parameter_analyses.analysis_parameter_name', 'like', '%' . $query['q'] . '%');
+                        });
+                    });
+
+                }
+            }
+        })
+        ->orderBy($orderBy, $ascending)
+        ->orderBy('parameter_analysis_groups.name', 'asc')
+        ->select('project_point_matrices.*')
+        ->get();
 
         return response()->json([
             'filter_result' => view('sample-analysis.parameter-analysis-result',
