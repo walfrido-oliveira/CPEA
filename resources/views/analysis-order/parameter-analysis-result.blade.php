8aefc04e2d02e7ed14826439bf979ff34c65875d
walfrido-oliveira <walfrido_15@hotmail.com>
2021-11-04 12:16:42 -0300

    correção

--- resources/views/analysis-order/parameter-analysis-result.blade.php
+++ resources/views/analysis-order/parameter-analysis-result.blade.php
@@ -5,8 +5,8 @@
 </thead>
 <tbody>
     @forelse ($projectPointMatrices as $index => $point)
-        @if (($index > 0 && $projectPointMatrices[$index]->pointIdentification->identification !=
-                            $projectPointMatrices[$index - 1]->pointIdentification->identification) || $index == 0)
+        @if (($index > 0 && $projectPointMatrices[$index]->point_identification_id !=
+              $projectPointMatrices[$index - 1]->point_identification_id) || $index == 0)
             <tr>
                 <td class="bg-gray-100 font-bold">
                     @if ($point->pointIdentification)
