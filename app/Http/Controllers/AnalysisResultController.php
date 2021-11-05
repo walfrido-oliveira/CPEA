2bfe65c13759a34734eac40e4bd2702df038485c
walfrido-oliveira <walfrido_15@hotmail.com>
2021-11-04 22:32:34 -0300

    correção

--- app/Http/Controllers/AnalysisResultController.php
+++ app/Http/Controllers/AnalysisResultController.php
@@ -517,7 +517,7 @@ class AnalysisResultController extends Controller
         {
             if($value->calculationParameters)
             {
-                dd($value->calculationParameter);
+                dd($value->calculationParameters);
                 $re = '/{(.*?)}/m';
                 $formula = $value->calculationParameters[0]->formula;
                 preg_match_all($re, $formula, $matches, PREG_SET_ORDER, 0);
