dfca4ab6994491046506373ae31fb15a102ed427
walfrido-oliveira <walfrido_15@hotmail.com>
2021-11-04 22:37:46 -0300

    correção

--- app/Http/Controllers/AnalysisResultController.php
+++ app/Http/Controllers/AnalysisResultController.php
@@ -524,6 +524,7 @@ class AnalysisResultController extends Controller
                 foreach ($matches as $key2 => $value2)
                 {
                     $result = explode("-", $value2[1]);
+                    dd($result);
                     $projectPointMatrix = $order->projectPointMatrices()->whereHas('parameterAnalysis', function($q) use($result) {
                         $q->where('parameter_analyses.analysis_parameter_name', $result[0])
                           ->where('parameter_analyses.cas_rn', $result[1]);
@@ -539,7 +540,6 @@ class AnalysisResultController extends Controller
                     }
                 }
 
-                dd($formula);
                 $stringCalc = new StringCalc();
                 $result = $stringCalc->calculate($formula);
 
