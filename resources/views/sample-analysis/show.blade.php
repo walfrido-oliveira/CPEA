dd3413903e0e2ff12901e812a0fbb90486fcdd68
walfrido-oliveira <walfrido_15@hotmail.com>
2021-11-24 14:43:50 -0300

    icone corrigido

--- resources/views/sample-analysis/show.blade.php
+++ resources/views/sample-analysis/show.blade.php
@@ -189,7 +189,7 @@
                             </a>
                         </div>
                         <x-spin-load />
-                        <div class="flex md:justify-end justify-center md:mx-0 mx-auto">
+                        <div class="flex md:justify-end justify-center md:mr-4 mx-auto ">
                             <div class="m-2 ">
                                 <button type="button" class="btn-outline-info" id="add-parameter-analysis-items">{{ __('Adicionar') }}</button>
                             </div>
@@ -200,7 +200,7 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" class="flex-1 w-6 h-6" style="-webkit-transform: scaleX(-1); transform: scaleX(-1);" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                                     </svg>
-                                    <span id="cart_amount" class="absolute left-0 top-0 rounded-full bg-green-600 w-4 h-4 top right p-0 m-0 text-white font-mono text-sm  leading-tight text-center">
+                                    <span id="cart_amount" class="absolute left-0 top-0 rounded-full bg-green-600 w-8 h-5 top right p-0 m-0 text-white font-mono text-sm  leading-tight text-center" style="left: -19px;top: 3px;">
                                         0
                                     </span>
                                 </a>
