a8a19f474446fe86ed05bc1fb5f7caa5914f5d6e
walfrido-oliveira <walfrido_15@hotmail.com>
2021-11-24 09:34:07 -0300

    correção

--- resources/views/project/campaign/show.blade.php
+++ resources/views/project/campaign/show.blade.php
@@ -28,14 +28,14 @@
 
                 <div class="py-2 my-2 bg-white rounded-lg min-h-screen">
                     <div class="flex md:flex-row flex-col w-full justify-end">
-                        <div class="md:mx-4 md:px-3 py-2  flex justify-end">
+                        <div class="flex justify-end">
                             <button type="button" class="btn-transition-primary" id="point_create" title="Adicionar novo ponto">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
                             </button>
                         </div>
-                        <div class="md:mx-4 md:px-3 py-2 flex justify-end" x-data="{ open: false }">
+                        <div class="md:mr-4 md:px-3 pr-2 flex justify-end" x-data="{ open: false }">
                             <div class="pr-4 flex">
                                 <button type="button" @click="open = !open" id="nav-toggle" class="w-full block btn-transition-secondary">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
