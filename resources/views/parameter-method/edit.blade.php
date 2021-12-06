b441acd8b620c1461dd27d68b3f66b759cc59400
walfrido-oliveira <walfrido_15@hotmail.com>
2021-12-06 12:48:03 -0300

    issue #80

--- resources/views/parameter-method/edit.blade.php
+++ resources/views/parameter-method/edit.blade.php
@@ -45,11 +45,11 @@
                     </div>
                     <div class="flex flex-wrap mx-4 px-3 py-2 mt-4">
                         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                            <x-jet-label for="time_preparation" value="{{ __('Prazo Preparo') }}" required/>
+                            <x-jet-label for="time_preparation" value="{{ __('Prazo Preparo (dias)') }}" required/>
                             <x-jet-input id="time_preparation" class="form-control block mt-1 w-full" type="number" name="time_preparation" maxlength="255" autofocus autocomplete="time_preparation" :value="$parameterMethod->time_preparation"/>
                             </div>
                         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                            <x-jet-label for="time_analysis" value="{{ __('Prazo Análise') }}" required/>
+                            <x-jet-label for="time_analysis" value="{{ __('Prazo Análise (dias)') }}" required/>
                             <x-jet-input id="time_analysis" class="form-control block mt-1 w-full" type="number" name="time_analysis" maxlength="255" autofocus autocomplete="time_analysis" :value="$parameterMethod->time_analysis"/>
                         </div>
                     </div>
