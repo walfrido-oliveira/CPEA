8b124fb7bbc0c77c1b4182c082f90a1e00b2dbb5
walfrido-oliveira <walfrido_15@hotmail.com>
2021-12-14 12:39:43 -0300

    issue #81

--- resources/views/sample-analysis/parameter-analysis-result.blade.php
+++ resources/views/sample-analysis/parameter-analysis-result.blade.php
@@ -37,18 +37,20 @@
                             ($projectPointMatrices[$index]->pointIdentification->identification !=
                             $projectPointMatrices[$index - 1]->pointIdentification->identification))
             <tr class="point-items-{{ $point->pointIdentification->id }}">
-                <td class="bg-gray-100" style="width: 1%; background-color:#e1ede1">
-                    <input class="form-checkbox parameter-analysis-group" type="checkbox"
-                           data-group-id="{{ $point->parameterAnalysis->parameterAnalysisGroup->id }}"
-                           data-identification-id="{{ $point->pointIdentification->id }}"
-                           value="{{ $point->parameterAnalysis->parameterAnalysisGroup->id }}">
-                </td>
-                <td colspan="5" class="font-bold text-black" style="background-color:#e1ede1">
-                    {{ $point->parameterAnalysis->parameterAnalysisGroup->name }}
-                    ({{ count($point->where("point_identification_id", $point->point_identification_id)->where('campaign_id', $point->campaign_id)->whereHas("parameterAnalysis", function($q) use($point) {
-                        $q->where("parameter_analysis_group_id", $point->parameterAnalysis->parameterAnalysisGroup->id);
-                    })->get()) }})
-                </td>
+                @if ($point->parameterAnalysis->parameterAnalysisGroup)
+                    <td class="bg-gray-100" style="width: 1%; background-color:#e1ede1">
+                        <input class="form-checkbox parameter-analysis-group" type="checkbox"
+                            data-group-id="{{ $point->parameterAnalysis->parameterAnalysisGroup->id }}"
+                            data-identification-id="{{ $point->pointIdentification->id }}"
+                            value="{{ $point->parameterAnalysis->parameterAnalysisGroup->id }}">
+                    </td>
+                    <td colspan="5" class="font-bold text-black" style="background-color:#e1ede1">
+                        {{ $point->parameterAnalysis->parameterAnalysisGroup->name }}
+                        ({{ count($point->where("point_identification_id", $point->point_identification_id)->where('campaign_id', $point->campaign_id)->whereHas("parameterAnalysis", function($q) use($point) {
+                            $q->where("parameter_analysis_group_id", $point->parameterAnalysis->parameterAnalysisGroup->id);
+                        })->get()) }})
+                    </td>
+                @endif
             </tr>
         @endif
 
