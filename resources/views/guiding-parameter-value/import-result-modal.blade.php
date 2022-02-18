a2f369b059e9bac73b2492df6b5ad8b596bf53d6
walfrido-oliveira <walfrido_15@hotmail.com>
2022-02-18 12:03:39 -0300

    correções

--- resources/views/guiding-parameter-value/import-result-modal.blade.php
+++ resources/views/guiding-parameter-value/import-result-modal.blade.php
@@ -60,12 +60,16 @@
                 </table>
               </div>
             </div>
+            <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                <button type="button" id="import_result_confirm_modal" class="w-full inline-flex justify-center btn-transition-secondary">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
           </div>
         </div>
         <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-          <button type="button" id="import_result_confirm_modal" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm">
-            {{ __('Fechar') }}
-          </button>
         </div>
       </div>
     </div>
