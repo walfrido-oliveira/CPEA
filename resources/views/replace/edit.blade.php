0f4dbed9e3350004110b1221387227580c95caac
walfrido-oliveira <walfrido_15@hotmail.com>
2022-04-01 12:25:55 -0300

    correção

--- resources/views/replace/edit.blade.php
+++ resources/views/replace/edit.blade.php
@@ -38,12 +38,8 @@
                     <div class="flex flex-wrap mx-4 px-3 py-2 mt-4">
                         <div class="w-full px-3 mb-6 md:mb-0">
                             <x-jet-label for="to" value="{{ __('Para') }}" required/>
-                            <button type="button" class="btn-transition-primary px-2 inline" id="to_create" title="Adicionar Para">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
-                            </button>
                             <x-custom-select :options="$to" name="to" id="to" :value="$replace->to"/>
+                            <small><a href="#" class="text-blue-500 underline" id="to_create" title="Adicionar Para">Adiciona novo Para</a></small>
                         </div>
                     </div>
                 </div>
