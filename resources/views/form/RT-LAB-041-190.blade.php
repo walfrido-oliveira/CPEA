f6d530736305828aba4f830a7ed616c905b51948
walfrido-oliveira <walfrido_15@hotmail.com>
2022-09-21 14:50:26 -0300

    correção listagem por lista

--- resources/views/form/RT-LAB-041-190.blade.php
+++ resources/views/form/RT-LAB-041-190.blade.php
@@ -85,30 +85,32 @@
                             @for ($i = 0; $i < count($formValue->values['samples']); $i++)
                                 @include('form.sample', ['sample' => $formValue->values['samples']["row_$i"]])
                             @endfor
-
-                            <div class="flex flex-wrap mt-2 w-full mode-list" style="display: none;">
-                                <div class="flex flex-wrap mt-2 w-full">
-                                    <div class="mx-1 p-3">
-                                        <p class="font-bold">{{ __('Ponto de Coleta') }}</p>
-                                        <p class="font-bold">{{ __('Condições ambientais nas últimas 24 hs') }}</p>
-                                        <p class="font-bold">{{ __('DT/HR da Coleta') }}</p>
-                                        <p style="background-color: #FFF; margin-left: -12px; margin-right: -12px; margin-top: 12px; margin-bottom: 12px;">&nbsp;</p>
-                                        <p class="font-bold">{{ __('Temperatura ºC') }}</p>
-                                        <p class="font-bold">{{ __('pH') }}</p>
-                                        <p class="font-bold">{{ __('ORP (mV)') }}</p>
-                                        <p class="font-bold">{{ __('Condutividade') }}</p>
-                                        <p class="font-bold">{{ __('Salinidade') }}</p>
-                                        <p class="font-bold">{{ __('Press.[psi]') }}</p>
-                                        <p class="font-bold">{{ __('Oxigênio Dissolvido (sat) (%)') }}</p>
-                                        <p class="font-bold">{{ __('Oxigênio Dissolvido (conc) (mg/L)') }}</p>
-                                        <p class="font-bold">{{ __('EH (mV)') }}</p>
-                                        <p class="font-bold">{{ __('Turbidez (NTU)') }}</p>
+                            @foreach (array_chunk($formValue->values['samples'], 3) as $sampleArray)
+                                <div class="flex flex-wrap mt-2 w-full mode-list" style="display: none;">
+                                    <div class="flex flex-wrap mt-2 w-full">
+                                        <div class="mx-1 p-3">
+                                            <p class="font-bold">{{ __('Ponto de Coleta') }}</p>
+                                            <p class="font-bold">{{ __('Condições ambientais nas últimas 24 hs') }}</p>
+                                            <p class="font-bold">{{ __('DT/HR da Coleta') }}</p>
+                                            <p style="background-color: #FFF; margin-left: -12px; margin-right: -12px; margin-top: 12px; margin-bottom: 12px;">&nbsp;</p>
+                                            <p class="font-bold">{{ __('Temperatura ºC') }}</p>
+                                            <p class="font-bold">{{ __('pH') }}</p>
+                                            <p class="font-bold">{{ __('ORP (mV)') }}</p>
+                                            <p class="font-bold">{{ __('Condutividade') }}</p>
+                                            <p class="font-bold">{{ __('Salinidade') }}</p>
+                                            <p class="font-bold">{{ __('Press.[psi]') }}</p>
+                                            <p class="font-bold">{{ __('Oxigênio Dissolvido (sat) (%)') }}</p>
+                                            <p class="font-bold">{{ __('Oxigênio Dissolvido (conc) (mg/L)') }}</p>
+                                            <p class="font-bold">{{ __('EH (mV)') }}</p>
+                                            <p class="font-bold">{{ __('Turbidez (NTU)') }}</p>
+                                        </div>
+                                        @for ($i = 0; $i < count($sampleArray); $i++)
+                                            @if(isset($sampleArray[$i]['results'])) @include('form.sample-list', ['sample' => $sampleArray[$i]]) @endif
+                                        @endfor
                                     </div>
-                                    @for ($i = 0; $i < count($formValue->values['samples']); $i++)
-                                        @include('form.sample-list', ['sample' => $formValue->values['samples']["row_$i"]])
-                                    @endfor
                                 </div>
-                            </div>
+                            @endforeach
+
                         @else
                             @include('form.sample')
                         @endif
