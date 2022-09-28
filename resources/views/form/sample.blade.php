41a0b681f713ad5a8647b34a0d2ba0973570499b
walfrido-oliveira <walfrido_15@hotmail.com>
2022-09-28 13:41:51 -0300

    correção no filtro

--- resources/views/form/sample.blade.php
+++ resources/views/form/sample.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full px-3 mt-4 mb-6 md:mb-0 flex flex-wrap sample @if(count(array_chunk($sample['results'], 3)) > 1) duplicates-table @else default-table @endif" id="sample_{{ isset($i) ? $i : 0 }}" data-index="1"
-    style="@if(count(array_chunk($sample['results'], 3)) > 1) display: none;@endif">
+     style="@if(count(array_chunk($sample['results'], 3)) > 1) display: none;@endif">
     <div class="flex w-full">
         <h3 class="w-full mt-4 mb-6 md:mb-0 title">AMOSTRA <span>{{ isset($i) ? $i + 1 : 1 }}</span></h3>
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0 flex justify-end align-baseline buttons" style="align-items: baseline;">
