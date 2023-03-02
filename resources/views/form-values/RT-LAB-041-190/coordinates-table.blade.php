34f1681268031691b4f051127681958943c402a8
walfrido-oliveira <walfrido_15@hotmail.com>
2023-03-02 11:51:04 -0300

    Formulário RT-LAB-041-190 - Pós Amostras Validas #222

--- resources/views/form-values/RT-LAB-041-190/coordinates-table.blade.php
+++ resources/views/form-values/RT-LAB-041-190/coordinates-table.blade.php
@@ -67,7 +67,7 @@
                                              name="{{ 'coordinates[' . $key . '][mn]' }}"/>
                             </td>
                             <td>
-                                <button type="button" class="btn-transition-primary remove-coodinate px-1" title="Remover Coodernada"  data-row="{{ $key }}">
+                                <button type="button" class="btn-transition-primary remove-coordinate px-1" title="Remover Coodernada"  data-row="{{ $key }}">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-wiph="1.5" stroke="currentColor" class="h-8 w-8 text-red-900">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                     </svg>
