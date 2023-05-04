aa0dddf90b06a59c92d51280f6c1bef635eca92d
walfrido-oliveira <walfrido_15@hotmail.com>
2023-05-04 12:25:05 -0300

    Formulário - RT-LAB-020/ RT-LAB-041-191- Ajustes #232

--- app/Http/Controllers/FormConfigController.php
+++ app/Http/Controllers/FormConfigController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Config;
 use Illuminate\Http\Request;
-use App\Http\Requests\ConfigRequest;
 use Illuminate\Support\Facades\Redirect;
 
 class FormConfigController extends Controller
